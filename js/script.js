6c68aadb5b70b5f74632007bf2b1f497fed9b6b1
Vadim1983 <Vshhukin@mail.ru>
2018-09-19 22:30:24 +0300

    ver.2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
-const body = document.body;
-let howMuchMoney = +prompt('Сколько у Вас с собой денег?');
-let howMuchApples = +prompt('Сколько Вы купили яблок?');
-let howMuchBreed = +prompt('Сколько Вы купили батонов хлеба??');
-let appleCost = +prompt('Сколько стоит одно яблоко?');
-let breadCost = +prompt('Сколько стоит один батон?');
-let totalAppleCost = howMuchApples * appleCost;
-let totalBreadCost = howMuchBreed * breadCost;
-let total = totalAppleCost + totalBreadCost;
-if (howMuchMoney >= total) {
-  body.innerHTML = '<p>true</p>';
-} else if (howMuchMoney < total) {
-  body.innerHTML = '<p>false</p>';
+let enterDigit = +prompt('Введите число');
+
+if (enterDigit == 0) {
+  console.log(enterDigit);
+} else {
+  console.log(Math.abs(enterDigit));
 }
